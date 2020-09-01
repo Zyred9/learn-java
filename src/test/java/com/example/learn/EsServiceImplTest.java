5b89e6a38ee8f4d565d50c2aa3f01d01108b141a
Zyred9 <1459656402@qq.com>
2020-09-01 18:09:14 +0800

    更换了es

--- src/test/java/com/example/learn/EsServiceImplTest.java
+++ src/test/java/com/example/learn/EsServiceImplTest.java
@@ -1,8 +1,13 @@
 package com.example.learn;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.learn.es.EsService;
 import com.example.learn.es.QueryPage;
-import com.example.learn.es.Student;
+import com.example.learn.es.Local;
+import com.example.learn.es.search.EsUtil;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,14 +20,16 @@ import java.util.List;
 public class EsServiceImplTest {
 
     @Autowired
+    private EsUtil esUtil;
+
+    @Autowired
     private EsService esService;
 
     @Test
     public void insert() {
-        List<Student> students = new ArrayList<>();
+        List<Local> students = new ArrayList<>();
         for (int i = 10; i <= 12; i++) {
-            Student student = new Student();
-            student.setId(i + "").setAge(10 + i).setName("王二狗" + i).setScore(72.5 + i).setInfo("大王派我来巡山" + i);
+            Local student = new Local();
             students.add(student);
         }
         esService.addAll(students);
@@ -32,7 +39,20 @@ public class EsServiceImplTest {
     public void fuzzySearch() {
         QueryPage queryPage = new QueryPage();
         queryPage.setCurrent(1).setSize(5);
-        Page<Student> list = esService.search("二狗2", queryPage);
+        Page<Local> list = esService.search("张永红", queryPage);
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void searchByUserIdAndName() {
+        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+        boolQueryBuilder.must(QueryBuilders.termQuery("username", "zyh"));
+        boolQueryBuilder.must(QueryBuilders.matchQuery("comment", "张永红"));
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.size(10).query(boolQueryBuilder);
+        List<JSONObject> search = esUtil.search(EsUtil.INDEX_NAME, builder, JSONObject.class);
+
+        search.stream().forEach(System.out::println);
+
     }
 }
